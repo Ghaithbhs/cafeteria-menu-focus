234c7bd3d6629a280c939ad2b6fcc968c4a54e15
Ghaith <ghaith_bhs@yahoo.fr>
2019-07-22 09:41:10 +0100

    ready for test

--- __init__.py
+++ __init__.py
@@ -4,7 +4,6 @@ from mycroft import MycroftSkill, intent_file_handler
 from datetime import date
 
 
-
 class CafeteriaMenu(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
@@ -37,7 +36,36 @@ class CafeteriaMenu(MycroftSkill):
             frid = self.settings.get("frid")
             self.speak_dialog("friday.menu", data={"frie": frie, "fris": fris, "frid": frid})
 
-"""
+    @intent_handler(IntentBuilder("").require("today.menu"))
+    def handle_friday_menu(self):
+        my_date = date.today()
+        today = my_date.get_weekday()  # calendar.day_name[my_date.weekday()]
+        if today == "monday":
+            mone = self.settings.get("mone")
+            mons = self.settings.get("mons")
+            mond = self.settings.get("mond")
+            self.speak_dialog("monday.menu", data={"mone": mone, "mons": mons, "mond": mond})
+        elif today == "tuesday":
+            tuee = self.settings.get("tuee")
+            tues = self.settings.get("tues")
+            tued = self.settings.get("tued")
+            self.speak_dialog("tuesday.menu", data={"tuee": tuee, "tues": tues, "tued": tued})
+        elif today == "wednesday":
+            wede = self.settings.get("wede")
+            weds = self.settings.get("weds")
+            wedd = self.settings.get("wedd")
+            self.speak_dialog("wednesday.menu", data={"wede": wede, "weds": weds, "wedd": wedd})
+        elif today == "thursday":
+            thue = self.settings.get("thue")
+            thus = self.settings.get("thus")
+            thud = self.settings.get("thud")
+            self.speak_dialog("thursday.menu", data={"thue": thue, "thus": thus, "thud": thud})
+        elif today == "friday":
+            frie = self.settings.get("frie")
+            fris = self.settings.get("fris")
+            frid = self.settings.get("frid")
+            self.speak_dialog("friday.menu", data={"frie": frie, "fris": fris, "frid": frid})
+
     @intent_handler(IntentBuilder("").require("tuesday.menu"))
     def handle_tuesday_menu(self):
         tuee = self.settings.get("tuee")
@@ -66,35 +94,6 @@ class CafeteriaMenu(MycroftSkill):
         frid = self.settings.get("frid")
         self.speak_dialog("friday.menu", data={"frie": frie, "fris": fris, "frid": frid})
 
-    @intent_handler(IntentBuilder("").require("today.menu"))
-    def handle_friday_menu(self):
-        my_date = date.today()
-        today = my_date.get_weekday() #calendar.day_name[my_date.weekday()]
-        if today == "monday":
-            mone = self.settings.get("mone")
-            mons = self.settings.get("mons")
-            mond = self.settings.get("mond")
-            self.speak_dialog("monday.menu", data={"mone": mone, "mons": mons, "mond": mond})
-        elif today == "tuesday":
-            tuee = self.settings.get("tuee")
-            tues = self.settings.get("tues")
-            tued = self.settings.get("tued")
-            self.speak_dialog("tuesday.menu", data={"tuee": tuee, "tues": tues, "tued": tued})
-        elif today == "wednesday":
-            wede = self.settings.get("wede")
-            weds = self.settings.get("weds")
-            wedd = self.settings.get("wedd")
-            self.speak_dialog("wednesday.menu", data={"wede": wede, "weds": weds, "wedd": wedd})
-        elif today == "thursday":
-            thue = self.settings.get("thue")
-            thus = self.settings.get("thus")
-            thud = self.settings.get("thud")
-            self.speak_dialog("thursday.menu", data={"thue": thue, "thus": thus, "thud": thud})
-        elif today == "friday":
-            frie = self.settings.get("frie")
-            fris = self.settings.get("fris")
-            frid = self.settings.get("frid")
-            self.speak_dialog("friday.menu", data={"frie": frie, "fris": fris, "frid": frid})"""
 
 def create_skill():
     return CafeteriaMenu()
